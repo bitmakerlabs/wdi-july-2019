0896496b3228f4ff0c886c1deafedcdda70d24b8
Fred Ngo <fredngo@gmail.com>
2019-07-26 11:36:00 -0400

    [19-mvc-migrations] Add views and templates for viewing contact

---
+++ 19-mvc-migrations/instructor/my_project/crm/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from crm.models import Contact
+
+def root_path(request):
+    return HttpResponseRedirect('contacts')
+
+def contacts_index(request):
+    context = { 'contacts': Contact.objects.all() }
+    response = render(request, 'contacts_index.html', context)
+    return HttpResponse(response)
