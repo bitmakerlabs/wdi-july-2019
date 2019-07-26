df1fe6ca0383f255752753026b0deed800ed3ec4
Fred Ngo <fredngo@gmail.com>
2019-07-26 11:59:08 -0400

    [19-mvc-migrations] Change note to notes

--- 19-mvc-migrations/instructor/my_project/crm/models.py
+++ 19-mvc-migrations/instructor/my_project/crm/models.py
@@ -4,10 +4,10 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
-    note = models.TextField()
+    notes = models.TextField()
 
     def __str__(self):
-        return f"full_name={self.full_name()}, email={self.email_address}, note={self.note}"
+        return f"full_name={self.full_name()}, email={self.email_address}, note={self.notes}"
 
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
