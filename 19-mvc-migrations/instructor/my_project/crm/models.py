8b12681a66ebff6a0267b001e1692d3397bdb7c4
Fred Ngo <fredngo@gmail.com>
2019-07-26 12:02:18 -0400

    [19-mvc-migrations] Undo note->notes

--- 19-mvc-migrations/instructor/my_project/crm/models.py
+++ 19-mvc-migrations/instructor/my_project/crm/models.py
@@ -4,10 +4,10 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
-    notes = models.TextField()
+    note = models.TextField()
 
     def __str__(self):
-        return f"full_name={self.full_name()}, email={self.email_address}, note={self.notes}"
+        return f"full_name={self.full_name()}, email={self.email_address}, note={self.note}"
 
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
