7787302cbbac83b6f30f8a564f46bc58ad4a6124
Fred Ngo <fredngo@gmail.com>
2019-07-26 11:23:45 -0400

    [19-mvc-migrations] Add str method to Contact

--- 19-mvc-migrations/instructor/my_project/crm/models.py
+++ 19-mvc-migrations/instructor/my_project/crm/models.py
@@ -6,5 +6,8 @@ class Contact(models.Model):
     email = models.CharField(max_length=255)
     note = models.TextField()
 
+    def __str__(self):
+        return f"full_name={self.full_name()}, email={self.email}, note={self.note}"
+
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
