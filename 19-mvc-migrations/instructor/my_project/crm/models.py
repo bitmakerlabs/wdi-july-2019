d30a7a1284fdbd7bd3f24824fb2a15d5030b6a52
Fred Ngo <fredngo@gmail.com>
2019-07-26 11:53:20 -0400

    [19-mvc-migrations] Rename email to email_address

--- 19-mvc-migrations/instructor/my_project/crm/models.py
+++ 19-mvc-migrations/instructor/my_project/crm/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Contact(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
     note = models.TextField()
 
     def __str__(self):
-        return f"full_name={self.full_name()}, email={self.email}, note={self.note}"
+        return f"full_name={self.full_name()}, email={self.email_address}, note={self.note}"
 
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
