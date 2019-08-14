44fb0c53aaf95deec67b88d011e2e4bb4bb1967e
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-14 10:33:27 -0400

    [28-apis-part-2] Add subapp to settings

--- 28-apis-part-2/instructor/moodtrip/moodtrip/settings.py
+++ 28-apis-part-2/instructor/moodtrip/moodtrip/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'moodtrip'
+    'moodtrip',
+    'trips'
 ]
 
 MIDDLEWARE = [
