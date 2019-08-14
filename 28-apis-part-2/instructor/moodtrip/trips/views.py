8b34ef984713b375519ede19fd9147719f440700
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-14 10:49:29 -0400

    [28-apis-part-2] Add trips index

--- 28-apis-part-2/instructor/moodtrip/trips/views.py
+++ 28-apis-part-2/instructor/moodtrip/trips/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from moodtrip.models import Trip
 
-# Create your views here.
+def index(request):
+    context = { 'trips': Trip.objects.all() }
+    response = render(request, 'index.html', context)
+    return HttpResponse(response)
+
+# def show(request, pk):
+#     pass
