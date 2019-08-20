0380860e4cf2d0d917b757465429685cec54b066
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-08-19 21:39:55 -0400

    [33-intro-to-ajax] Add 'Goals' and 'Stretch Goals'

--- 33-intro-to-ajax/student/ajax.js
+++ 33-intro-to-ajax/student/ajax.js
@@ -1,3 +1,12 @@
 window.addEventListener('DOMContentLoaded', function () {
+  // GOALS:
+  // - 1. Allow users to fetch and display monster data.
+  // - 2. Allow users to submit new monster data.
+  //
+  // STRETCH GOALS:
+  // - 3. Prevent users from submitting duplicate monsters.
+  // - 4. Display an error message if a given request fails.
+  // - 5. Automatically display newly created monster data.
+
   // TODO: Add code here!
 });
