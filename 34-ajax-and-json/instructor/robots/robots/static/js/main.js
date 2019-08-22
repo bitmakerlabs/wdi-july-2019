e9d330846b3e50b392182ddc993321346ee5a451
Jeremy Tupper <jeremytupper1@gmail.com>
2019-08-22 12:30:58 -0400

    Extra commented info from ajax and json class

--- 34-ajax-and-json/instructor/robots/robots/static/js/main.js
+++ 34-ajax-and-json/instructor/robots/robots/static/js/main.js
@@ -1,25 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let robotLinks = document.querySelectorAll('.robots__robot a');
+document.addEventListener('DOMContentLoaded', function() {
+  const robotLinks = document.querySelectorAll('.robots__robot a');
+  const robotBox = document.querySelector('.robots__box__details');
 
-  robotLinks.forEach(link => {
-    link.addEventListener('click', e => {
+  robotLinks.forEach(robotLink => {
+    robotLink.addEventListener('click', function(e) {
       e.preventDefault();
-      axios.get(link.href, { headers: { 'X-Requested-With' : 'XMLHttpRequest' } })
-        .then(response => {
-          let data = response.data;
-          let pTag = document.createElement('p');
-          pTag.innerText = data.name;
-          let robotBox = document.querySelector('.robots__box__details');
+      axios
+        .get(robotLink.href, { headers: { 'X-Requested-With': 'XMLHttpRequest' } })
+        .then(response => response.data)
+        // .then(function(response) {
+        //   const { data } = response;
+        //   return data
+        // })
+        .then(data => {
+          // console.log(data);
+          console.log({ data });
+
+          const nameTag = document.createElement('h3');
+          nameTag.innerText = data.name;
+          const modelTag = document.createElement('p');
+          modelTag.innerText = data.model_number;
           robotBox.innerHTML = '';
-          robotBox.appendChild(pTag);
+          robotBox.appendChild(nameTag);
+          robotBox.appendChild(modelTag);
         })
         .catch(error => {
-          console.error(error);
+          console.log(error);
         });
-    })
-  })
-
-  // robotLinks.forEach(function (link) {
-  //
-  // })
+    });
+  });
 });
