4cbfb678d46a45bf383e5b198ddcedc2aad545f1
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2019-08-23 10:11:31 -0400

    Undo solution code

--- 35-ajax-and-forms/instructor/robots/robots/static/js/main.js
+++ 35-ajax-and-forms/instructor/robots/robots/static/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let robotLinks = document.querySelectorAll('.robots__robot a.robot-name');
 
-/*  robotLinks.forEach(link => {
+  robotLinks.forEach(link => {
     link.addEventListener('click', e => {
       e.preventDefault();
       axios.get(link.href, { headers: { 'X-Requested-With' : 'XMLHttpRequest' } })
@@ -17,43 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => {
           console.error(error);
         });
-    })
-  })*/
-
-  robotLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      let html = axios.get(link.href);
-      document.querySelector('#robot-details-container').innerHTML = html;
-    })
-  })  
-
-  let newRobotLink = document.querySelector('#new-robot-link');
-
-  newRobotLink.addEventListener('click', e => {
-    e.preventDefault();
-    let html = axios.get('/robots/new')
-    document.querySelector('#robot-form-container').innerHTML = html;
-  })
-
-  let robotForm = document.querySelector('.robot-form');
-
-  robotForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let url = robotForm.action;
-    let csrf = robotForm.querySelector('input[name=csrfmiddlewaretoken]').value;
-    axios({
-      method: 'post',
-      url: url, 
-      data: {
-        name: robotForm.querySelector('#robot-name').value,
-        model_number: robotForm.querySelector('#robot-model-number').value,
-        address: robotForm.querySelector('#robot-address').value
-      },
-      headers: {"X-CSRFToken": csrf}
-    })
-    .then(function (response) {
-      console.log(response)
     })
   })
 
