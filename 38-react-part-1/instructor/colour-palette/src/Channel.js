da2f26f85df04676558f247a65f3523d5d95c6ce
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-28 12:07:07 -0400

    [38-react-part-1] Add event handlers

--- 38-react-part-1/instructor/colour-palette/src/Channel.js
+++ 38-react-part-1/instructor/colour-palette/src/Channel.js
@@ -12,11 +12,24 @@ import React from 'react';
 //const {key1, key2, key3, key4} = props
 
 const Channel = ({value}) => {
+
+  const handleUp = () => {
+    console.log('+1')
+  }
+
+  const handleDown = () => {
+    console.log('-1')
+  }
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="channel">
-      <button type="button" className="btn up">+</button>
-      <input type="text" className="txt" value={value} />
-      <button type="button" className="btn down">-</button>
+      <button type="button" className="btn up" onClick={ handleUp }>+</button>
+      <input type="text" className="txt" value={value} onChange={ handleChange } />
+      <button type="button" className="btn down" onClick={ handleDown }>-</button>
     </div>
   )
 }
