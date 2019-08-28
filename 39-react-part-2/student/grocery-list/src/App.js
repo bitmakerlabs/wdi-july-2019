1f04007cf4fdf0b86fbafdcd29d60048f22d82cc
Fred Ngo <fredngo@gmail.com>
2019-08-28 08:57:41 -0400

    [39-react-part-2] Use class fields instead

--- 39-react-part-2/student/grocery-list/src/App.js
+++ 39-react-part-2/student/grocery-list/src/App.js
@@ -3,25 +3,25 @@ import Form from './Form';
 import Filters from './Filters';
 import List from './List';
 
-const filters = [
-  { name: 'All', value: 'all' },
-  { name: 'Meat', value: 'meat' },
-  { name: 'Produce', value: 'prod' },
-  { name: 'Dairy', value: 'dairy' },
-  { name: 'Bakery', value: 'bakery' },
-];
-
-const initialItems = [
-  { name: 'Steak', type: 'meat', quantity: 3 },
-  { name: 'Apples', type: 'prod', quantity: 4 },
-  { name: 'Milk (1L, 2%)', type: 'dairy', quantity: 1 },
-  { name: 'Baguettes', type: 'bakery', quantity: 2 },
-];
-
 class App extends Component {
 
+  filters = [
+    { name: 'All', value: 'all' },
+    { name: 'Meat', value: 'meat' },
+    { name: 'Produce', value: 'prod' },
+    { name: 'Dairy', value: 'dairy' },
+    { name: 'Bakery', value: 'bakery' },
+  ];
+
+  initialItems = [
+    { name: 'Steak', type: 'meat', quantity: 3 },
+    { name: 'Apples', type: 'prod', quantity: 4 },
+    { name: 'Milk (1L, 2%)', type: 'dairy', quantity: 1 },
+    { name: 'Baguettes', type: 'bakery', quantity: 2 },
+  ];
+
   state = {
-    items: initialItems
+    items: this.initialItems
   }
 
   incrementItemQuantity = (index) => {
@@ -59,7 +59,7 @@ class App extends Component {
           <h1>Grocery List</h1>
         </header>
         <Form />
-        <Filters filters={filters}/>
+        <Filters filters={this.filters}/>
         <List
           items={this.state.items}
           incrementItem={this.incrementItemQuantity}
