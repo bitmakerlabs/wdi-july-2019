204690a1f88c51c873b14280a354fbad06a0f083
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-29 11:39:22 -0400

    [39-react-part-2] Use refs to grab values from input fields

--- 39-react-part-2/instructor/colour-palette/src/Form.js
+++ 39-react-part-2/instructor/colour-palette/src/Form.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 const Form = () => {
+  const rRef = React.createRef();
+  const gRef = React.createRef();
+  const bRef = React.createRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(
+      rRef.current.value,
+      gRef.current.value,
+      bRef.current.value,      
+    )
+  }
+
   return (
-    <form>
-      <input type="number" />
-      <input type="number" />
-      <input type="number" />
-      <button>Add Colour!</button>      
+    <form onSubmit={handleSubmit} >
+      <input ref={rRef} type="number" />
+      <input ref={gRef} type="number" />
+      <input ref={bRef} type="number" />
+      <button>Add Colour!</button>
     </form>
   )
 }
