9435ca964ee8c1abd626e14369f5b37ad3889ab8
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-29 10:32:26 -0400

    [39-react-part-2] Use a map to render child components

--- 39-react-part-2/instructor/airbnb/src/App.js
+++ 39-react-part-2/instructor/airbnb/src/App.js
@@ -4,10 +4,27 @@ import './App.css';
 import Listing from './Listing';
 
 const App = () => {
+
+  const initialListings = [
+    {
+      title: "My Cozy House",
+      description: "Clean and modern",
+      views: 100
+    },
+    {
+      title: "Cute Cottage",
+      description: "Cozy and clean",
+      views: 50
+    },
+  ]
+
+  const listingElements = initialListings.map(
+    (listing) => <Listing title={listing.title} description={listing.description} views={listing.views} />
+  )
+
   return (
     <div className="App">
-      <Listing title="My Cozy House" description="Clean and modern" views={100} />
-      <Listing title="Cute Cottage" description="Cozy and clean" views={50} />
+      { listingElements }
     </div>
   );
 }
