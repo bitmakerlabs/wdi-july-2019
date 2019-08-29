9945bd818220f5f2beb18fe8e55ea1f58d2983b1
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-08-29 11:56:42 -0400

    Convert Palette t class, add swatches state

--- 39-react-part-2/instructor/colour-palette/src/Palette.js
+++ 39-react-part-2/instructor/colour-palette/src/Palette.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Swatch from './Swatch';
 import Form from './Form';
 
-const Palette = () => {
-  const initialSwatches = [
+class Palette extends Component {
+
+  initialSwatches = [
     { red: 255, green: 0, blue: 0 },
     { red: 0, green: 255, blue: 0 },
     { red: 0, green: 0, blue: 255 },
   ]
 
-  const addSwatch = (s) => {
-    console.log(`Adding a swatch`, s);
-    initialSwatches.push(s)
-
-    console.log(initialSwatches)
+  state = {
+    swatches: this.initialSwatches
   }
 
-  const swatchElements = initialSwatches.map(
-    (s, i) => <Swatch key={i} {...s} />
-  )
+  addSwatch = (s) => {
+    this.setState(
+      // (prevState) => {
+      //   const newSwatches = [...prevState.swatches, s]
+      //   const newState = { swatches: newSwatches }
+      //   return newState
+      // }
+      // ---- same as ----
+      (prevState) => ({ swatches: [...prevState.swatches, s] })
+    )
+  }
 
-  return (
-    <>
-      <ul className="palette">
-        { swatchElements }
-      </ul>
-      <Form onSubmit={ addSwatch }/>
-    </>
-  );
+  render() {
+    const swatchElements = this.state.swatches.map(
+      (s, i) => <Swatch key={i} {...s} />
+    )
+
+    return (
+      <>
+        <ul className="palette">
+          { swatchElements }
+        </ul>
+        <Form onSubmit={ this.addSwatch }/>
+      </>
+    );
+  }
 };
 
 export default Palette;
