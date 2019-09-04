63f6c97e4936a2dce593c0365e759bb87d9b08d5
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-09-04 10:30:37 -0400

    [42-react-part-5] Add routes

--- 42-react-part-5/instructor/dentist/src/App.js
+++ 42-react-part-5/instructor/dentist/src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Procedures from './pages/Procedures'
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-      <Procedures />
-      <Contact />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/procedures" component={Procedures} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
