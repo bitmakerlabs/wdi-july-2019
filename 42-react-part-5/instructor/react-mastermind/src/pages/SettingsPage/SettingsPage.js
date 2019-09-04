0c69f788ea06d164eff2de494c6ceae1c61d3dd6
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2019-09-04 11:57:39 -0400

    [42-react-part-5] Add Home link

--- 42-react-part-5/instructor/react-mastermind/src/pages/SettingsPage/SettingsPage.js
+++ 42-react-part-5/instructor/react-mastermind/src/pages/SettingsPage/SettingsPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const SettingsPage = (props) => {
   return (
-    <h1>Settings</h1>
+    <>
+      <Link to="/">Home</Link>
+      <h1>Settings</h1>
+    </>
   )
 }
 
